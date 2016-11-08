b0af7e41d2c58e134dfcc9e66534cc049cf4079c
Tyler Roush <m0n5terenergy@aol.com>
2016-11-08 01:30:48 -0500

    Add current arduino files to be used for testing

---
+++ jawTest.ino
@@ -0,0 +1,67 @@
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_DCMotor *jawMotor = AFMS.getMotor(1);
+
+int jawPot = 0;
+int maxJaw;
+int minJaw;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Creepy face control software");
+  Serial.println("Calibrating potentiometers");
+  AFMS.begin();
+  jawMotor->setSpeed(255);
+  jawMotor->run(FORWARD);
+  delay(1000);
+  maxJaw = analogRead(jawPot);
+  Serial.println("Maximum jaw position: " + String(maxJaw));
+  jawMotor->run(RELEASE);
+  delay(100);
+  jawMotor->run(BACKWARD);
+  delay(1000);
+  minJaw = analogRead(jawPot);
+  Serial.println("Minimum jaw position: " + String(minJaw));
+  jawMotor->run(RELEASE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    Serial.println("Ready to accept input");
+    int currentVal = analogRead(jawPot);
+    int targetVal = Serial.readString().toInt();
+    if (targetVal < currentVal)
+    {
+      jawMotor->setSpeed(255);
+      jawMotor->run(BACKWARD);
+      while (true)
+      {
+        currentVal = analogRead(jawPot);
+        if (currentVal <= targetVal)
+        {
+          jawMotor->run(RELEASE);
+          break;
+        }
+      }
+    }
+    else if(targetVal > currentVal)
+    {
+      jawMotor->setSpeed(255);
+      jawMotor->run(FORWARD);
+      while (true)
+      {
+        currentVal = analogRead(jawPot);
+        if (currentVal >= targetVal)
+        {
+          jawMotor->run(RELEASE);
+          break;
+        }
+      }
+    }
+  }
+}
---
+++ serialTest.ino
@@ -0,0 +1,61 @@
+byte incomingBytes[3] = {0, 0, 0};
+int working_Part = 0;
+int target_Speed = 0;
+int target_Position = 0;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    //Attempt to read in our command
+    read_Command();
+    switch(working_Part)
+    {
+      case 0:
+        Serial.print("Move jaw to ");
+        Serial.print(target_Position, DEC);
+        Serial.print(" at speed: ");
+        Serial.println(target_Speed, DEC);
+        break;
+      case 1:
+        Serial.print("Move lip to ");
+        Serial.print(target_Position, DEC);
+        Serial.print(" at speed: ");
+        Serial.println(target_Speed, DEC);
+        break;
+      case 2:
+        Serial.print("Move eyes to ");
+        Serial.print(target_Position, DEC);
+        Serial.print(" at speed: ");
+        Serial.println(target_Speed, DEC);
+        break;
+      case 3:
+        Serial.print("Move eyebrows to ");
+        Serial.print(target_Position, DEC);
+        Serial.print(" at speed: ");
+        Serial.println(target_Speed, DEC);
+        break;
+      default:
+        Serial.println("Unrecognized command");
+        break;
+    }
+  }
+}
+
+//Functions to catch and process our incoming bytes for our move commands
+//May contain a lot of redundency but are mostly here to increase readability
+void read_Command()
+{
+  Serial.readBytes(incomingBytes, 3);
+  working_Part = incomingBytes[0];
+  target_Speed = incomingBytes[1];
+  target_Position = incomingBytes[2];
+}
+
