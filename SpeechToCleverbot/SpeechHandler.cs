ff4457bb5ecee662dfcc63bc268e6b3e9d6794d2
Tyler Roush <m0n5terenergy@aol.com>
2016-11-08 17:54:01 -0500

    Added chat functionality files

---
+++ SpeechToCleverbot/SpeechHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cleverbot.Net;
+using RobotChat;
+
+namespace RobotChat
+{
+    class SpeechHandler
+    {
+        private SpeechRecognitionEngine recognizer;
+        private SpeechSynthesizer synth;
+        private CleverbotSession session;
+
+        public SpeechHandler()
+        {
+            session = Cleverbot.createSession();
+            recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+            synth = new SpeechSynthesizer();
+
+            recognizer.SetInputToDefaultAudioDevice();
+            synth.SetOutputToDefaultAudioDevice();
+
+            DictationGrammar defaultDictationGrammar = new DictationGrammar();
+            defaultDictationGrammar.Name = "default dictation";
+            defaultDictationGrammar.Enabled = true;
+            recognizer.LoadGrammar(defaultDictationGrammar);
+
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+        }
+
+        public void Begin_Listening()
+        {
+            while (true)
+            {
+                recognizer.Recognize();
+            }
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine("Speech recognized: " + e.Result.Text);
+            var response = Cleverbot.getChatResponse(session, e.Result.Text);
+            Console.WriteLine("Cleverbot: " + response);
+            synth.Speak(response);
+        }
+    }
+}
